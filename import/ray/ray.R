a34848dd3fe56b374cdb87588bd6c2a9119dac70
Holger Döring <doering@uni-bremen.de>
2015-12-05 16:54:29 +0100

    more coherent use of dplyr and code revision  * replace base R function with dplyr equivalent if available  * more explicit construction of unique party id  * code style guide: ' --> "  * creates updated import data (change of variable names)

--- import/ray/ray.R
+++ import/ray/ray.R
@@ -2,24 +2,25 @@ library("foreign")
 library("dplyr")
 library("stringr")
 
+rm(list = ls())
+
 # get local copy of data file
 url <- "http://www.lsu.edu/faculty/lray2/data/1996survey/1996survey.sav?export=sav"
 data_file_local <- "source__1996survey.sav"
 if( ! file.exists(data_file_local)) {
-  download.file(url, data_file_local, mode='wb')
+  download.file(url, data_file_local, mode="wb")
 }
 
 # read, select and clean data
-ray <- read.spss("source__1996survey.sav", to.data.frame = TRUE) %>%
-  select(NATID:CMPCODE) %>%  # select party information
-  mutate_each(funs(str_trim), NATION, PARTY, ENAME, NAME)  # trim whitespaces
+ray_raw <- read.spss("source__1996survey.sav", to.data.frame = TRUE)
+ray <- ray_raw %>%
+  select(NATID:CMPCODE, -NATION) %>%  # select party information
+  mutate_each(funs(str_trim), PARTY, ENAME, NAME) %>%  # trim white space
+  mutate(id = as.integer(NATID) * 1000 + PARTYID,  # create unique party id
+         natid_name = as.character(NATID), NATID = as.integer(NATID))
 
 # add country ISO codes
-country <- read.csv('../country.csv', as.is=TRUE)
-ray <- merge(ray, country[ , c('name', 'iso3')], by.x='NATID', by.y='name', all.x=TRUE)
+country <- read.csv("../country.csv", as.is=TRUE) %>% select(name, iso3)
+ray <- ray %>% left_join(country, by = c("natid_name" = "name")) %>% arrange(iso3, PARTY)
 
-# create unique party id from country id and party id
-ray$NATID <- as.integer(ray$NATID)
-ray$id <- as.integer(ray$NATID) * 1000 + ray$PARTYID
-
-write.csv(ray, 'ray.csv', na='', row.names = FALSE, fileEncoding='utf-8')
+write.csv(ray, "ray.csv", na="", row.names = FALSE, fileEncoding="utf-8")
