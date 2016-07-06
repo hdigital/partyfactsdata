ff7df9f97c84d27158be28e5c7b1701eec63d30d
Holger Döring <doering@uni-bremen.de>
2016-07-06 18:54:35 +0200

    code revision for Thomas (1975)
    using `dplyr` more coherently

--- import/thomas/thomas.R
+++ import/thomas/thomas.R
@@ -1,19 +1,26 @@
-library('dplyr')
+library("dplyr")
 
-thomas <- read.csv('thomas-parties.csv', fileEncoding='utf-8', as.is=TRUE)
-country <- read.csv('../country.csv', fileEncoding = 'utf-8', as.is = TRUE)
+thomas_raw <- read.csv("thomas-parties.csv", fileEncoding = "utf-8", as.is = TRUE)
+country_raw <- read.csv("../country.csv", fileEncoding = "utf-8", as.is = TRUE)
+
+# remove 'status quo' information
+thomas <- thomas_raw %>% filter(party != "Status quo")
 
 # replace UK and RUS with proper country names
 thomas$country[thomas$country == "Great Britain"] <- "United Kingdom"
 thomas$country[thomas$country == "USSR"] <- "Russia"
 
+# aggregate parties first and last year
 thomas <- thomas %>%
-  filter(party != "Status quo") %>%
   group_by(country, party) %>%
-  summarise(year_first = min(time), year_last = max(time)) %>%
-  left_join(country %>% select(name, name_short), by = c('country' = 'name')) %>%
-  select(name = party, year_first, year_last, country, country_short=name_short)
+  summarise(year_first = min(time), year_last = max(time)) %>% 
+  ungroup()
 
-thomas$id <- 1:nrow(thomas)
+# get country name iso shortcut
+country <- country_raw %>% select(country_short = name_short, country = name)
+thomas <- thomas %>%
+  left_join(country) %>%
+  arrange(country, party) %>% 
+  mutate(id = row_number())
 
-write.csv(thomas, 'thomas.csv', na='', row.names = FALSE, fileEncoding='utf-8')
+write.csv(thomas, "thomas.csv", na = "", row.names = FALSE, fileEncoding = "utf-8")
