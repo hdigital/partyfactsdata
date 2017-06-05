ba17226152d5e51849ac275cf14818f4a09d8c86
Holger Döring <doering@uni-bremen.de>
2017-06-05 13:13:45 +0200

    use tidyverse for Ray import

--- import/ray/ray.R
+++ import/ray/ray.R
@@ -1,6 +1,6 @@
-library("foreign")
-library("dplyr")
+library("tidyverse")
 library("stringr")
+library("haven")
 
 # get local copy of data file
 url <- "http://www.lsu.edu/faculty/lray2/data/1996survey/1996survey.sav?export=sav"
@@ -10,15 +10,14 @@ if( ! file.exists(data_file_local)) {
 }
 
 # read, select and clean data
-ray_raw <- read.spss("source__1996survey.sav", to.data.frame = TRUE)
+ray_raw <- read_spss("source__1996survey.sav")
 ray <- ray_raw %>%
-  select(NATID:CMPCODE, -NATION) %>%  # select party information
+  select(NATID:CMPCODE) %>%  # select party information
   mutate_each(funs(str_trim), PARTY, ENAME, NAME) %>%  # trim white space
-  mutate(id = as.integer(NATID) * 1000 + PARTYID,  # create unique party id
-         natid_name = as.character(NATID), NATID = as.integer(NATID))
+  mutate(id = as.integer(NATID) * 1000 + PARTYID)  # create unique party id
 
 # add country ISO codes
-country <- read.csv("../country.csv", as.is=TRUE) %>% select(name, iso3)
-ray <- ray %>% left_join(country, by = c("natid_name" = "name")) %>% arrange(iso3, PARTY)
+country <- read_csv("../country.csv") %>% select(name, iso3)
+ray <- ray %>% left_join(country, by = c("NATION" = "name")) %>% arrange(iso3, PARTY)
 
-write.csv(ray, "ray.csv", na="", row.names = FALSE, fileEncoding="utf-8")
+write_csv(ray, "ray.csv", na = "")
