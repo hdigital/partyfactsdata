295454aa6d76bd3ef95187cef2e6d4335e1c32a6
Holger Döring <doering@uni-bremen.de>
2017-06-15 17:23:16 +0200

    fix minor country issues in Huber and Janda  * Janda -- recode GDR into DDR  * Huber -- shorter ISO3 country variable name

--- import/huber/huber.R
+++ import/huber/huber.R
@@ -5,7 +5,7 @@ huber_raw <- read_csv("huber_inglehart_1995.csv")
 
 huber <- huber_raw %>%
   rename(huber_id=id) %>%
-  mutate(country_name_short = countrycode(country, "country.name", "iso3c",
-                                          custom_match = c(`NORTHERN IRELAND`="NIR")))
+  mutate(country_short = countrycode(country, "country.name", "iso3c",
+                                     custom_match = c(`NORTHERN IRELAND`="NIR")))
 
 write_csv(huber, "huber.csv", na = "")
--- import/janda/janda.R
+++ import/janda/janda.R
@@ -3,15 +3,18 @@ library(tidyverse)
 janda <- read_csv("janda-parties.csv")
 country <- read_csv("janda-country.csv")
 
-# Extract country id from party id
+# extract country id from party id
 janda <- janda %>%
   mutate(country_id = substr(janda_id, 1, nchar(janda_id) - 1) %>% as.integer,
          country_id = ifelse(janda_id >= 10, country_id, 0))  # add US country id "0"
 
-# Merge parties and country list
+# merge parties and country list
 janda <- janda %>%
   left_join(country, by = c("country_id" = "id")) %>%
   select(-country_id, country_short = short) %>%
   filter(country_short != "")
+
+# correct iso3 country names
+janda[janda$country_short == "GDR", "country_short"] <- "DDR"
 
 write_csv(janda, "janda.csv", na = "")
