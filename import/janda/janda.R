1516dc539709fc4adc31359d92254bf28504592a
Holger Döring <doering@uni-bremen.de>
2018-11-11 20:26:02 +0100

    fix Janda country issues (DDR, MWI)

--- import/janda/janda.R
+++ import/janda/janda.R
@@ -5,16 +5,14 @@ country <- read_csv("janda-country.csv")
 
 # extract country id from party id
 janda <- janda %>%
-  mutate(country_id = substr(janda_id, 1, nchar(janda_id) - 1) %>% as.integer,
-         country_id = ifelse(janda_id >= 10, country_id, 0))  # add US country id "0"
+  mutate(
+    country_id = substr(janda_id, 1, nchar(janda_id) - 1) %>% as.integer,
+    country_id = ifelse(janda_id >= 10, country_id, 0)  # add US country id "0"
+    )
 
 # merge parties and country list
 janda <- janda %>%
   left_join(country, by = c("country_id" = "id")) %>%
-  select(-country_id, country_short = short) %>%
-  filter(country_short != "")
-
-# correct iso3 country names
-janda[janda$country_short == "GDR", "country_short"] <- "DDR"
+  select(-country_id, country_short = short)
 
 write_csv(janda, "janda.csv", na = "")
