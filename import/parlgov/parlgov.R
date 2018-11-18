1463f2e6793ffc2e6aa2e732671faa61d90bfc71
Holger Döring <doering@uni-bremen.de>
2018-11-18 13:37:56 +0100

    ParlGov import revision - add number of elections and cabinets - remove small parties - update reference

--- import/parlgov/parlgov.R
+++ import/parlgov/parlgov.R
@@ -1,56 +1,102 @@
 library(tidyverse)
 library(dbplyr)
 
-url <- "http://www.parlgov.org/static/data/parlgov-development.db"
+
+# Get/read data ----
+
 db_file <- "source__parlgov.db"
 if( ! file.exists(db_file)) {
+  url <- "http://www.parlgov.org/static/data/parlgov-development.db"
   download.file(url, db_file, mode = "wb")
 }
 
 con <- DBI::dbConnect(RSQLite::SQLite(), db_file)
 tbl_parlgov <- function(table) tbl(con, table) %>% as_tibble()
 
-party <- tbl_parlgov("view_party")
-elec <- tbl_parlgov("view_election")
+party_pg <- tbl_parlgov("view_party")
+elec_pg <- tbl_parlgov("view_election")
+cab_pg <- tbl_parlgov("view_cabinet")
 party_raw <- tbl_parlgov("party")
 
+
+# Summarize information ----
+
 # calculate first and last year each party
-elec_year <- elec %>%
-  mutate(year = substr(elec$election_date, 1, 4)) %>%
+elec_year <- elec_pg %>%
+  mutate(year = substr(election_date, 1, 4)) %>%
   group_by(party_id) %>%
   summarise(first = min(year, na.rm = TRUE), last = max(year, na.rm = TRUE)) %>%
   select(party_id, first, last)
 
 # calculate max vote share each party
-elec_share <- elec %>%
-  filter(election_type == "parliament") %>%
-  group_by(party_id) %>%
-  mutate(vote_share_max = max(vote_share, na.rm = TRUE)) %>%
-  filter(vote_share == vote_share_max) %>%
-  distinct(party_id, .keep_all = TRUE) %>%
-  ungroup() %>%
-  mutate(vote_share_max = round(vote_share_max, 1),
-         vote_share_max_year = substr(election_date, 1, 4)) %>%
-  select(party_id, vote_share_max_year, vote_share_max)
+get_elec_share <- function(elec_type = "parliament") {
+  elec_share <- elec_pg %>%
+    filter(election_type == elec_type) %>%
+    group_by(party_id) %>%
+    mutate(
+      election_n = n(),
+      seats = ifelse(seats >= 2, seats, 0),  # ignore 1 seats parties
+      share = ifelse(vote_share, vote_share, 100*seats/seats_total),
+      share_max = max(share, na.rm = TRUE)
+      ) %>%
+    filter(share == share_max) %>%
+    distinct(party_id, .keep_all = TRUE) %>%
+    ungroup() %>%
+    mutate(
+      share_max = round(share_max, 1),
+      share_max_year = substr(election_date, 1, 4)
+    ) %>%
+    select(party_id, election_n, share_max_year, share_max)
+}
 
+elec_share_parl <- get_elec_share("parliament")
+elec_share_ep <- get_elec_share("ep") %>% filter(! party_id %in% elec_share_parl$party_id)
+elec_share <- elec_share_parl %>% bind_rows(elec_share_ep)
+
+# number of cabinet memberships
+cab_n <- 
+  cab_pg %>% 
+  filter(cabinet_party == 1) %>% 
+  count(party_id) %>% 
+  rename(cabinet_n = n)
+
+# party information
 parlgov_url <- "http://www.parlgov.org/explore/%s/party/%d/"
-parlgov <- party %>%
+party_info <- 
+  party_pg %>%
   select(party_id, country_name_short:family_name, -country_name, -party_name_ascii, -family_name) %>%
   filter(family_name_short != "none") %>%
-  mutate(url = sprintf(parlgov_url, tolower(country_name_short), party_id)) %>%
+  mutate(url = sprintf(parlgov_url, tolower(country_name_short), party_id)) %>% 
+  rename(country = country_name_short, family = family_name_short)
+  
+
+# Create dataset ----
+
+# Combine ParlGov information
+parlgov <- 
+  party_info %>% 
   left_join(elec_year) %>%
+  left_join(cab_n) %>%
   left_join(elec_share) %>%
   left_join(party_raw %>% select(id, wikipedia), by = c("party_id" = "id")) %>%
-  arrange(country_name_short, party_name)
+  arrange(country, party_name)
 
 # recode Denmark autonomous regions
 parlgov <- parlgov %>% 
   mutate(
-    country_name_short = case_when(
+    country = case_when(
       str_detect(party_name_english, fixed("(Faroe Islands)")) ~ "FRO",
       str_detect(party_name_english, fixed("(Greenland)")) ~ "GRL",
-      TRUE ~ country_name_short
+      TRUE ~ country
     )
   )
 
-write_csv(parlgov, "parlgov.csv", na = "")
+
+# Select parties ----
+
+# specify filter criteria to ignore small parties
+parlgov_out <- 
+  parlgov %>%
+  filter(share_max >= 1 | election_n >= 3 | cabinet_n >= 1)
+
+write_csv(parlgov_out, "parlgov.csv", na = "")
--- import/parlgov/readme.md
+++ import/parlgov/readme.md
@@ -1,9 +1,8 @@
 ## Source
 
-Döring, Holger and Philip Manow. 2018. Parliaments and governments database (ParlGov): Information on parties, elections and cabinets in modern democracies. Development version.
-
-http://www.parlgov.org/
+Döring, Holger and Philip Manow. 2018. Parliaments and governments database (ParlGov): Information on parties, elections and cabinets in modern democracies. Development version. – [parlgov.org](http://www.parlgov.org/)
 
 ## Import
 
-see `parlgov.R`
+* see `parlgov.R`
+* excluding smaller parties (< 1% share)
