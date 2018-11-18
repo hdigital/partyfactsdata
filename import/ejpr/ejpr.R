c98ef1b91a35875ffa610ac6372633b838184add
Holger Döring <doering@uni-bremen.de>
2018-11-18 13:34:30 +0100

    fix EJPR-PDY year issue (1899)

--- import/ejpr/ejpr.R
+++ import/ejpr/ejpr.R
@@ -1,21 +1,27 @@
 library(tidyverse)
+library(glue)
 library(readxl)
 
 get_ejpr_parties <- function(file_path) {
   party <- read_excel(file_path, sheet = "info_parties2")
   names(party)[1] <- "ejpr_id"
   names(party) <- tolower(names(party))
-  party <- party[ , 1:8] %>% filter(ejpr_id != 0, ! is.na(ejpr_id))
-  return(party)
+  party %>%
+    select(1:8) %>%
+    filter(ejpr_id != 0, ! is.na(ejpr_id))
 }
 
-path <- "source__ejpr-pdy/"
-party <- bind_rows(lapply(paste0(path, list.files(path)), get_ejpr_parties))
+sources_path <- glue("source__ejpr-pdy/{list.files('source__ejpr-pdy')}")
+party <- map(sources_path, get_ejpr_parties) %>% bind_rows()
 
-party_out <- party %>%
+party_out <-
+  party %>%
   filter(max_vote >= 0.01, ! is.na(max_vote)) %>%
-  mutate(max_vote = round(max_vote * 100, 1),
-         country = toupper(country)) %>%
-  mutate_at(vars(contains("year")), funs(substr(., 1, 4) %>% as.integer))
+  mutate_at(vars(contains("year")), funs(substr(., 1, 4) %>% as.integer)) %>%
+  mutate(
+    max_vote = round(max_vote * 100, 1),
+    country = toupper(country),
+    first_pdy_year = case_when(first_pdy_year == 1899 ~ NA_integer_, TRUE ~ first_pdy_year)
+  )
 
 write_csv(party_out, "ejpr.csv", na = "")
