bda7697fc38024afe19436cf6ab9742f865ac1a1
pbederke <42916617+pbederke@users.noreply.github.com>
2019-05-11 03:25:38 +0200

    migrate mapp parties from Party Facts web

---
+++ import/mapp/mapp.R
@@ -0,0 +1,84 @@
+library(tidyverse)
+library(countrycode)
+
+raw_mapp <- readxl::read_xlsx("source__MAPP_dataset_-_Version_2.0.xlsx", sheet = 1)
+
+# dataset with Party Facts and ParlGov IDs
+if(FALSE) {
+  read.csv("partyfacts-external-parties.csv") %>% 
+    filter(dataset_key == "parlgov") %>%
+    select(partyfacts_id, parlgov_id = dataset_party_id) %>% 
+    write_csv("partyfacts-parlgov.csv")
+}
+
+raw_pfext <- read.csv("partyfacts-parlgov.csv") %>% 
+  mutate(
+    parlgov_id = as.character(parlgov_id)
+  )
+
+
+# Extract information ----
+mapp_temp <- raw_mapp %>%
+  select(
+    `Party ID (MAPP)`, `PartyID (ParlGov)`, Country, `Party acronym`,
+    `Full name (original language)`, `Full name (English translation)`,
+    `Year founded`, `Year disappearance`
+  ) %>%
+  mutate(
+    country_short = countrycode(
+      Country,
+      origin = "country.name",
+      destination = "iso3c"
+    ),
+    party_id = paste(`Party ID (MAPP)`),
+    name = case_when(
+      country_short == "CYP" ~ NA_character_,
+      TRUE ~ `Full name (original language)`
+    )
+  )
+
+# add partyfacts_id via parlgov_id's
+mapp_temp2 <- mapp_temp %>% 
+  left_join(raw_pfext, by = c("PartyID (ParlGov)" = "parlgov_id")) %>%
+  select(
+    country_short, `Party acronym`, name,
+    `Full name (English translation)`, `Year founded`, `Year disappearance`,
+    party_id, partyfacts_id
+  ) %>%
+  group_by(party_id) %>%
+  slice(1L) %>%  # keep only once the party_id of MAPP
+  ungroup()      # although party had different names
+
+
+# rename columns
+colnames(mapp_temp2) <- c(
+  "country", "name_short", "name", "name_english",
+  "year_first", "year_last", "party_id",
+  "partyfacts_id"
+)
+
+# fix unicode issue
+mapp <- mapp_temp2 %>% 
+  mutate(
+    name = case_when(
+      country == "ROU" & name_short %in% c("PPPS", "FSN") ~ NA_character_,
+      country == "CYP" & name_short == "EDEK" ~ NA_character_,
+      TRUE ~ name
+    )
+  )
+
+
+write.csv(mapp, "mapp.csv", row.names = FALSE, fileEncoding = "UTF-8", na = "")
+
+
+## Data checks ----
+
+# check wether there are any duplicates or differences in the number of
+# party_id after the manipulation
+duplicated(mapp$party_id) %>% any()
+
+raw_mapp %>%
+  mutate(party_id = paste(`Party ID (MAPP)`)) %>%
+  moonBook::mytable()
+
+moonBook::mytable(mapp)
---
+++ import/mapp/readme.md
@@ -0,0 +1,10 @@
+# MAPP - Data Party Membership Figures
+
+## Source
+
++ van Haute, Emilie, & Paulis, Emilien. MAPP Dataset [Data set]. Zenodo. http://doi.org/10.5281/zenodo.61234
+
+## Import
+
++ Dataset was mostly linked via the ParlGov IDs and existing PartyFacts ID
++ The original name for parties of CYP has been removed because of encoding issues.
