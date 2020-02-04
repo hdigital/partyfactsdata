824831beab0b7ad09dbfa46f768606f7369a4a41
pbederke <42916617+pbederke@users.noreply.github.com>
2020-02-05 00:21:26 +0100

    migrate latino parties from Party Facts web

---
+++ import/latino/latino.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+## load latino-parties
+latino_parties <- read_csv("latino-parties.csv")
+
+## filter share 
+latino <- latino_parties %>% 
+  filter(share >= 5)
+
+## check for duplicates
+duplicated(latino$party_id) %>% any()
+
+## write csv
+write_csv(latino, "latino.csv", na = "")
