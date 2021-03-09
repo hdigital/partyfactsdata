c7223ee67f52a4cb87a08bf27174d31f8bb22dda
pbederke <42916617+pbederke@users.noreply.github.com>
2021-03-09 12:47:29 +0100

    Revert "update Manifesto Project"
    This reverts commit 59da23a893106ef205e495d8cfb3823ae9b67c2a.

--- import/manifesto/manifesto.R
+++ import/manifesto/manifesto.R
@@ -9,18 +9,9 @@ manifesto <- manifesto_raw %>% select(-country) %>% inner_join(manifesto_share)
 # add Party Facts country codes
 country_custom <- c(`German Democratic Republic` = "DDR",
                     `Northern Ireland` = "NIR")
-
 manifesto <- manifesto %>%
-  mutate(country = countrycode(
-    countryname, "country.name", "iso3c", custom_match = country_custom
-    ),
-    dummy_nir = if_else(countryname == "NIR", 1, 0),
-    countryname = case_when(
-      countryname == "NIR" ~ "GBR",
-      TRUE ~ countryname
-    )
-  )
-
+  mutate(country = countrycode(countryname, "country.name", "iso3c",
+                               custom_match = country_custom))
 if(any(is.na(manifesto$country))) {
   warning("Country name clean-up needed")
 }
@@ -28,4 +19,4 @@ if(any(is.na(manifesto$country))) {
 # replace party short longer than 25 chars
 manifesto[nchar(manifesto$abbrev) > 25 & ! is.na(manifesto$abbrev), "abbrev"] <- NA
 
-write_csv(manifesto, "manifesto.csv", na = "")
+write.csv(manifesto, "manifesto.csv", na = "", fileEncoding = "utf-8", row.names = FALSE)
