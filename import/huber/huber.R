f86b56f1ecf4df9dbcb68f3ad8e402418cfafde3
pbederke <42916617+pbederke@users.noreply.github.com>
2021-03-09 14:17:40 +0100

    remove Northern Ireland (#44)
    * update Manifesto Project
    
    * update Huber / Inglehart (1995)
    
    * update PPMD (2006)
    
    * update Wikipedia
    
    * Revert "update Manifesto Project"
    
    This reverts commit 59da23a893106ef205e495d8cfb3823ae9b67c2a.
    
    * update Manifesto Project
    
    * correct Huber

--- import/huber/huber.R
+++ import/huber/huber.R
@@ -5,7 +5,14 @@ huber_raw <- read_csv("huber_inglehart_1995.csv")
 
 huber <- huber_raw %>%
   rename(huber_id=id) %>%
-  mutate(country_short = countrycode(country, "country.name", "iso3c",
-                                     custom_match = c(`NORTHERN IRELAND`="NIR")))
+  mutate(country_short = countrycode(
+    country, "country.name", "iso3c", custom_match = c(`NORTHERN IRELAND`="NIR")
+    ),
+    dummy_nir = if_else(country_short == "NIR", 1, 0),
+    country_short = case_when(
+      country_short == "NIR" ~ "GBR",
+      TRUE ~ country_short
+    )
+  )
 
 write_csv(huber, "huber.csv", na = "")
