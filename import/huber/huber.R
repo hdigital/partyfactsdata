f0fa8ddd846f95f0c6b8cf04558b004498061e3c
pbederke <42916617+pbederke@users.noreply.github.com>
2021-03-09 12:56:26 +0100

    correct Huber

--- import/huber/huber.R
+++ import/huber/huber.R
@@ -8,10 +8,10 @@ huber <- huber_raw %>%
   mutate(country_short = countrycode(
     country, "country.name", "iso3c", custom_match = c(`NORTHERN IRELAND`="NIR")
     ),
-    dummy_nir = if_else(country == "NIR", 1, 0),
-    country = case_when(
-      country == "NIR" ~ "GBR",
-      TRUE ~ country
+    dummy_nir = if_else(country_short == "NIR", 1, 0),
+    country_short = case_when(
+      country_short == "NIR" ~ "GBR",
+      TRUE ~ country_short
     )
   )
 
