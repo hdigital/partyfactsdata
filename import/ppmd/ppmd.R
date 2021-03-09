b8abf48d400bdde6365fa79819a5e60b230ab816
pbederke <42916617+pbederke@users.noreply.github.com>
2021-03-09 12:32:22 +0100

    update PPMD (2006)

--- import/ppmd/ppmd.R
+++ import/ppmd/ppmd.R
@@ -6,11 +6,18 @@ ppmd_raw <- read_csv("source__ppmd_summary_data.csv")
 
 # one observation per party and unique
 country_custom = c(`NI`= "NIR", `SR` = "SRB", `UK` = "GBR")
+
 ppmd <- ppmd_raw %>% 
   mutate(
     id = paste(Country, Party, sep = "-"),
-    country_short = countrycode(Country, "iso2c", "iso3c", 
-                                custom_match = country_custom)
+    country_short = countrycode(
+      Country, "iso2c", "iso3c", custom_match = country_custom
+      ),
+    dummy_nir = if_else(country_short == "NIR", 1, 0),
+    country_short = case_when(
+      country_short == "NIR" ~ "GBR",
+      TRUE ~ country_short
+    )
     ) %>% 
   distinct(Country, Party, .keep_all = TRUE) %>%
   arrange(Country, Party)
