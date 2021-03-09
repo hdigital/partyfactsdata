1e2b767bdcd13544b14b97cc302b74f4fb586270
pbederke <42916617+pbederke@users.noreply.github.com>
2021-03-09 12:29:42 +0100

    update Huber / Inglehart (1995)

--- import/huber/huber.R
+++ import/huber/huber.R
@@ -5,7 +5,14 @@ huber_raw <- read_csv("huber_inglehart_1995.csv")
 
 huber <- huber_raw %>%
   rename(huber_id=id) %>%
-  mutate(country_short = countrycode(country, "country.name", "iso3c",
-                                     custom_match = c(`NORTHERN IRELAND`="NIR")))
+  mutate(country_short = countrycode(
+    country, "country.name", "iso3c", custom_match = c(`NORTHERN IRELAND`="NIR")
+    ),
+    dummy_nir = if_else(country == "NIR", 1, 0),
+    country = case_when(
+      country == "NIR" ~ "GBR",
+      TRUE ~ country
+    )
+  )
 
 write_csv(huber, "huber.csv", na = "")
