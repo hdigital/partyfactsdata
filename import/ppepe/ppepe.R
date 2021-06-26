6cec7697afbc5bdd304f5ce7bac08f87fa84ba83
Holger Döring <doering@uni-bremen.de>
2021-06-26 21:28:50 +0200

    migrate ppepe from PF-Web

---
+++ import/ppepe/ppepe.R
@@ -0,0 +1,49 @@
+library(countrycode)
+library(tidyverse)
+
+raw_ppepe <- read_csv("source__ppepe.csv")
+
+
+## Prepare data ----
+
+ppepe <- 
+  raw_ppepe %>% 
+  mutate(country_short = countrycode(country, origin = 'country.name', destination = 'iso3c')) %>% 
+  group_by(country_short, party_name_short) %>% 
+  mutate(
+    first = min(year),
+    last = max(year),
+    share_max = max(vote_share),
+    share_year = if_else(vote_share == share_max, year, NA_real_)
+  ) %>% 
+  ungroup()
+
+
+## Import dataset ----
+
+ppepe_out <- 
+  ppepe %>% 
+  filter(! is.na(share_year)) %>% 
+  distinct(country_short, party_name_short, .keep_all = TRUE) %>% 
+  mutate(
+    party_id = row_number(),
+    party_short = str_replace(party_name_short, "( \\+ )(.+)", "")
+    ) %>% 
+  relocate(country_short, party_id, party_short, .before = country) %>% 
+  select(country_short:party_eng, first:share_year)
+
+write_csv(ppepe_out, "ppepe.csv")
+
+
+## Figure ----
+
+pl <- 
+  ppepe_out %>%
+  mutate(country = fct_infreq(country) %>% fct_rev()) %>% 
+  ggplot(aes(x = country)) +
+  geom_bar() +
+  coord_flip() +
+  theme(axis.title.x = element_blank(), axis.title.y = element_blank()) 
+
+print(pl)
+ggsave("ppepe.png", pl, width = 20, height = 15, units = "cm")
---
+++ import/ppepe/readme.md
@@ -0,0 +1,8 @@
+# PPEPE
+
++ Zulianello, Mattia, and Erik Gahner Larsen. 2021. “Populist Parties in European Parliament Elections: A New Dataset on Left, Right and Valence Populism from 1979 to 2019.” Electoral Studies 71: 102312. doi: [10.1016/j.electstud.2021.102312](https://www.doi.org/10.1016/j.electstud.2021.102312)
++ Larsen, Erik Gahner, and Mattia Zulianello. 2021. “Populist Parties in European Parliament Elections.” Harvard Dataverse, V1, UNF:6:yQK5WxJrC5re7OPJbeJbOw== [fileUNF] — doi: [10.7910/DVN/RFRCZS](https://www.doi.org/10.7910/DVN/RFRCZS)
+
+---
+
+![](ppepe.png)
