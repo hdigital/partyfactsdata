8bb4c5695a85c4dcee0726afc1015d72726d05fa
pbederke <42916617+pbederke@users.noreply.github.com>
2022-02-28 18:54:36 +0100

    migrate NPED from Party Facts web (#57)

---
+++ import/nped/nped.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+
+file_in <- read_csv("nped.csv", na = "")
+
+# add name_short & party_id
+file_out <- 
+  file_in %>% 
+  mutate(
+    name_short = str_remove_all(name_english, "[[:lower:]*[:space:]*[:punct:]*]"),
+    party_id = paste(country, name_short, year_first, sep = "-"),
+    party_id = case_when(
+      name_english == "Francophone Socialist Party" ~ "BEL-FSP-1978-1",
+      name_english == "Flemish Socialist Party" ~ "BEL-FSP-1978-2",
+      TRUE ~ party_id
+    )
+  )
+
+# check for duplicate party_id
+duplicated(file_out$party_id) %>% any()
+
+write_csv(file_out, "nped.csv", na = "")
---
+++ import/nped/readme.md
@@ -0,0 +1,9 @@
+# New Party Entrance Dataset (NPED)
+
+## Source
+
++ Bolin, Niklas. 2012. Målsättning riksdagen. Ett aktörsperspektiv på nya partiers inträde i det nationella parlamentet [Making the Breakthrough. An Agency-Centred Perspective on New Party Entrance into National Parliaments]. Umeå: Umeå University
+
+## Import
+
++ Parties manually extracted from the codebook (PDF)
