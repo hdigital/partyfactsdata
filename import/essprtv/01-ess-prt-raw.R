a29c263e981e21ac0b208009b267f563a417bd0e
Holger Döring <doering@uni-bremen.de>
2023-04-01 12:44:59 +0200

    Use auto-format R code in RStudio

--- import/essprtv/01-ess-prt-raw.R
+++ import/essprtv/01-ess-prt-raw.R
@@ -26,25 +26,31 @@ ess_dta_files <-
 # function to get party name and party ID
 get_ess_parties <- function(data, encoding) {
   data_path <- paste0(ess_dta_path, data)
-  
-  party <- 
+
+  party <-
     read.dta13(data_path, fromEncoding = encoding) %>%
     select(cntry, essround, starts_with(c("prtv", "prtc"))) %>%
-    pivot_longer(c(-cntry, -essround), names_to = "variable", values_to = "party")
-  
-  party_id <- 
-    read.dta13(data_path, convert.factors = FALSE, fromEncoding = encoding) %>%
+    pivot_longer(c(-cntry, -essround),
+                 names_to = "variable",
+                 values_to = "party")
+
+  party_id <-
+    read.dta13(data_path,
+               convert.factors = FALSE,
+               fromEncoding = encoding) %>%
     select(cntry, essround, starts_with(c("prtv", "prtc"))) %>%
-    pivot_longer(c(-cntry, -essround), names_to = "variable", values_to = "party_id") %>% 
+    pivot_longer(c(-cntry, -essround),
+                 names_to = "variable",
+                 values_to = "party_id") %>%
     pull(party_id)
-  
+
   party["party_id"] <- party_id
-  
+
   return(party)
 }
 
 # party name and party ID for round 1-9 -- time intense so avoiding rereading
-if(! exists("ess_prt_raw")) {
+if (!exists("ess_prt_raw")) {
   ess_prt_raw <-
     pmap(ess_dta_files, ~ get_ess_parties(..1, ..2), .progress = TRUE) %>%
     bind_rows()
@@ -58,7 +64,7 @@ ess_prt_out <-
   ess_prt_raw %>%
   drop_na(party) %>%
   distinct() %>%
-  mutate(ess_id = paste(cntry, essround, party_id, substr(variable, 4, 4), sep = "-")) %>% 
+  mutate(ess_id = paste(cntry, essround, party_id, substr(variable, 4, 4), sep = "-")) %>%
   arrange(cntry, essround, variable, party_id)
 
 write_csv(ess_prt_out, "01-ess-prt-raw.csv", na = "")
@@ -67,23 +73,23 @@ write_csv(ess_prt_out, "01-ess-prt-raw.csv", na = "")
 ## Data issues ESS ----
 
 print("ESS variables with multipe prtv* variables per county -- duplicate 'ess_id'")
-ess_prt_out %>% 
-  filter(str_detect(variable, "prtv.+\\d")) %>% 
-  pull(variable) %>% 
-  unique() %>% 
+ess_prt_out %>%
+  filter(str_detect(variable, "prtv.+\\d")) %>%
+  pull(variable) %>%
+  unique() %>%
   paste(collapse = ", ")
 
 
 # find parties with different ids in prtv/prtc
 
-prt_check <- 
-  ess_prt_out %>% 
-  select(ess_id, variable, party) %>% 
-  mutate(variable = substr(variable, 1, 4)) %>% 
-  distinct(ess_id, variable, .keep_all = TRUE) %>% 
+prt_check <-
+  ess_prt_out %>%
+  select(ess_id, variable, party) %>%
+  mutate(variable = substr(variable, 1, 4)) %>%
+  distinct(ess_id, variable, .keep_all = TRUE) %>%
   pivot_wider(names_from = variable, values_from = party)
 
-prt_check_diff <- 
-  prt_check %>% 
+prt_check_diff <-
+  prt_check %>%
   drop_na() %>%
   filter(prtv != prtc)
--- import/essprtv/03-essprt.R
+++ import/essprtv/03-essprt.R
@@ -7,20 +7,29 @@ raw_ess_clean <- read_csv("02-ess-harmonize.csv")
 
 ## Party Facts information ----
 
-if(! exists("pf_party")) {  # time intense so avoiding rereading
+if (!exists("pf_party")) {
+  # time intense so avoiding rereading
   url <- "https://partyfacts.herokuapp.com/download"
-
-  raw_pf_core <- read_csv(glue("{url}/core-parties-csv/"), na = "")
-  raw_pf_ext <- read_csv(glue("{url}/external-parties-csv/"), na = "", guess_max = 50000)
   
-  pf_ext <- 
-    raw_pf_ext %>% 
-    filter(str_starts(dataset_key, "essprt")) %>% 
+  raw_pf_core <- read_csv(glue("{url}/core-parties-csv/"), na = "")
+  raw_pf_ext <-
+    read_csv(glue("{url}/external-parties-csv/"),
+             na = "",
+             guess_max = 50000)
+  
+  pf_ext <-
+    raw_pf_ext %>%
+    filter(str_starts(dataset_key, "essprt")) %>%
     select(partyfacts_id, first_ess_id = dataset_party_id)
-
-  pf_party <- 
-    raw_pf_core %>% 
-    select(country, name_short, name, name_english, partyfacts_id, technical) %>% 
+  
+  pf_party <-
+    raw_pf_core %>%
+    select(country,
+           name_short,
+           name,
+           name_english,
+           partyfacts_id,
+           technical) %>%
     inner_join(pf_ext)
 }
 
@@ -28,56 +37,61 @@ if(! exists("pf_party")) {  # time intense so avoiding rereading
 ## ESS preparation ----
 
 # add some variables needed below
-prt <- 
-  raw_ess_clean %>% 
+prt <-
+  raw_ess_clean %>%
   mutate(
     prt_var = str_extract(ess_variable, "prtv|prtc"),
     year = essround * 2 + 2000,
     essround = paste0("R", essround)
-    ) %>% 
-  unite(ess_info, essround, ess_variable, ess_party, sep = " --- ", remove = FALSE) %>% 
+  ) %>%
+  unite(ess_info,
+        essround,
+        ess_variable,
+        ess_party,
+        sep = " --- ",
+        remove = FALSE) %>%
   select(-essround, -ess_variable)
 
 # collapse ESS rounds at the party level
-prt_info <- 
-  prt %>% 
-  group_by(prt_var, first_ess_id) %>% 
+prt_info <-
+  prt %>%
+  group_by(prt_var, first_ess_id) %>%
   summarise(
     first_ess_name = first(ess_party),
     year_first = min(year),
     year_last = max(year),
     ess_ids = paste(ess_id, collapse = " "),
     ess_info = paste(ess_info, collapse = "\n")
-  ) %>% 
+  ) %>%
   mutate(ess_ids = str_replace_all(ess_ids, " +", " "))
 
-prt_out <- 
-  prt %>% 
-  distinct(first_ess_id) %>% 
-  inner_join(prt_info) %>% 
-  inner_join(pf_party) %>% 
+prt_out <-
+  prt %>%
+  distinct(first_ess_id) %>%
+  inner_join(prt_info) %>%
+  inner_join(pf_party) %>%
   relocate(partyfacts_id, .before = country)
-  
+
 
 ## Link files ----
 
 # question vote intention (prtv*)
-prt_out %>% 
-  filter(prt_var == "prtv") %>% 
-  select(-prt_var) %>% 
+prt_out %>%
+  filter(prt_var == "prtv") %>%
+  select(-prt_var) %>%
   write_csv("essprtv.csv", na = "")
 
 # question party close to (prtc*)
-prt_out %>% 
-  filter(prt_var == "prtc") %>% 
-  select(-prt_var) %>% 
+prt_out %>%
+  filter(prt_var == "prtc") %>%
+  select(-prt_var) %>%
   write_csv("../essprtc/essprtc.csv", na = "")
 
 # all ESS prt* variables
-prt_out_all <- 
-  raw_ess_clean %>% 
-  left_join(pf_party) %>% 
-  relocate(partyfacts_id, .before = country) %>% 
+prt_out_all <-
+  raw_ess_clean %>%
+  left_join(pf_party) %>%
+  relocate(partyfacts_id, .before = country) %>%
   arrange(ess_cntry, essround, ess_variable, ess_party_id)
 
 write_csv(prt_out_all, "essprt-all.csv", na = "")
@@ -85,17 +99,15 @@ write_csv(prt_out_all, "essprt-all.csv", na = "")
 
 ## Plot ESS-prtv  ----
 
-pl_dt <- 
+pl_dt <-
   prt_out_all %>%
-  filter(is.na(technical), str_ends(ess_id, "v")) %>% 
-  distinct(country, essround, ess_id) %>% 
-  count(country, essround, sort = TRUE) %>% 
-  mutate(
-    country = factor(country) %>% fct_rev(),
-    essround = factor(essround)
-  )
+  filter(is.na(technical), str_ends(ess_id, "v")) %>%
+  distinct(country, essround, ess_id) %>%
+  count(country, essround, sort = TRUE) %>%
+  mutate(country = factor(country) %>% fct_rev(),
+         essround = factor(essround))
 
-pl <- 
+pl <-
   ggplot(pl_dt, aes(y = country, x = essround, size = n)) +
   geom_point(colour = "darkgrey") +
   scale_y_discrete(name = "") +
@@ -106,4 +118,8 @@ pl <-
   guides(color = F)
 
 # plot(pl)
-ggsave("essprtv.png", pl, width = 20, height = 25, units = "cm")
+ggsave("essprtv.png",
+       pl,
+       width = 20,
+       height = 25,
+       units = "cm")
