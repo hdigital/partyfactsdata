f74d67ce714e400ccadb5759c1b23a6c3ef06a45
pbederke <42916617+pbederke@users.noreply.github.com>
2023-04-07 22:26:07 +0200

    Migrate TAESD from Party Facts web

---
+++ import/taesd/readme.md
@@ -0,0 +1,7 @@
+# Thin Anti-Establishment Supply Dataset (TAESD)
+
+## Source
+
++ Pytlas, B. (2022). Beyond populism: The diversity of thin anti-establishment contestation in turbulent times. Party Politics, 0(0) 1-13. [https://doi.org/10.1177/13540688221080536](https://doi.org/10.1177/13540688221080536) 
+
++ Pytlas, B (2022). Thin Anti-Establishment Supply Dataset (TAESD). [https://doi.org/10.17605/OSF.IO/F23HM](https://doi.org/10.17605/OSF.IO/F23HM)
---
+++ import/taesd/taesd.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(countrycode)
+
+file_in <- readxl::read_xls("source__TAESD_Thin_Antiestablishment_Supply_Dataset_V_1_0_1220_xls.xls", na = "")
+
+file_out <- 
+  file_in %>% 
+  group_by(id) %>% 
+  mutate(
+    countrycode = countrycode(country, "iso2c", "iso3c"),
+    year = as.numeric(paste0(20, str_extract(year, "[:digit:]{2}"))),
+    year_first = min(year, na.rm = T),
+    year_last = max(year, na.rm = T),
+    max_share = max(vote, na.rm = T),
+    share_year = if_else(vote == max_share, year, NA_real_),
+    max_share = round(max_share, 3) * 100,
+  ) %>% 
+  filter(!is.na(share_year)) %>% 
+  select(countrycode, year_first, year_last, name_short = party, year, vote_share = max_share, party_id = id)
+
+
+file_out$party_id %>% duplicated() %>% any()
+
+write_csv(file_out, "taesd.csv", na = "")
