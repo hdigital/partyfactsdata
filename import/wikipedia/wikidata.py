d271db152447c6285960af093a137749a502b22b
Holger Döring <doering@uni-bremen.de>
2023-06-05 22:05:13 +0200

    Review and revise code
    + use black code formatter
    + remove unused imports
    + use short dataframe name to avoid long lines
    + short long lines

--- import/wikipedia/wikidata.py
+++ import/wikipedia/wikidata.py
@@ -4,51 +4,61 @@
 """
 
 import pandas as pd
-import numpy as np
 import requests
 from urllib.parse import unquote
 from tqdm import tqdm
 
 
 # Load Data
-partyfacts = pd.read_csv("import/wikipedia/wikipedia.csv")
-
+pf_raw = pd.read_csv("wikipedia-data.csv")
+pf = pf_raw
 
 # Pre-process wikipedia URL
-partyfacts['wiki_title'] = partyfacts.url.str.extract(r"wiki/(.*$)", expand=False)
-partyfacts['wiki_title'] = partyfacts.wiki_title.apply(unquote)
-partyfacts['wiki_server'] = partyfacts.url.apply(lambda x: x.split('/')[2])
+pf["wiki_title"] = pf.url.str.extract(r"wiki/(.*$)", expand=False)
+pf["wiki_title"] = pf.wiki_title.apply(unquote)
+pf["wiki_server"] = pf.url.apply(lambda x: x.split("/")[2])
 
 
 # Default Query Parameters
-params = {'action': 'query',
-          'prop': 'pageprops',
-          'ppprop': 'wikibase_item',
-          'redirects': 1,
-          'titles': '',
-          'format': 'json'}
+params = {
+    "action": "query",
+    "prop": "pageprops",
+    "ppprop": "wikibase_item",
+    "redirects": 1,
+    "titles": "",
+    "format": "json",
+}
 
 # Initialize empty column
-partyfacts['wikidata_id'] = ""
+pf["wikidata_id"] = ""
 
 # query wikidata for ID
-for index, row in tqdm(partyfacts.iterrows(), total=len(partyfacts), desc="Getting wikidata IDs"):
+tqdm_iterrows = tqdm(pf.iterrows(), total=len(pf), desc="Getting wikidata IDs")
+
+for index, row in tqdm_iterrows:
     api = f"https://{row['wiki_server']}/w/api.php"
-    params['titles'] = row['wiki_title']
+    params["titles"] = row["wiki_title"]
     r = requests.get(api, params=params)
     j = r.json()
     try:
-        for val in j['query']['pages'].values():
-            wikidata_id = val['pageprops']['wikibase_item']
+        for val in j["query"]["pages"].values():
+            wikidata_id = val["pageprops"]["wikibase_item"]
             break
     except:
         wikidata_id = ""
-    partyfacts.at[index, 'wikidata_id'] = wikidata_id
+    pf.at[index, "wikidata_id"] = wikidata_id
 
 
 # prepare for export
-keep_cols = ['country', 'partyfacts_id', 'wikidata_id', 'name', 'name_short', 'name_native']
-partyfacts = partyfacts.loc[:,keep_cols]
+keep_cols = [
+    "country",
+    "partyfacts_id",
+    "wikidata_id",
+    "name",
+    "name_short",
+    "name_native",
+]
+pf = pf.loc[:, keep_cols]
 
 # save as csv
-partyfacts.reset_index(drop=True).to_csv('import/wikipedia/wikidata.csv', index = False)
+pf.reset_index(drop=True).to_csv("import/wikipedia/wikidata.csv", index=False)
