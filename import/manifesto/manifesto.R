b487b4da951cf0795adacec7ca5b88634b86ea91
polvis <polvis@users.noreply.github.com>
2023-12-03 11:28:33 +0100

    update manifesto 2023a (#66)
    * update manifesto to 2023a version
    
    * Add suggestions code revision
    
    + use RStudio code formatter
    + use Base-R pipe
    + use Base-R shorthand anonymous functions
    + use single line for each pipe step
    
    ---------
    
    Co-authored-by: Holger Döring <doering@uni-bremen.de>

--- import/manifesto/manifesto.R
+++ import/manifesto/manifesto.R
@@ -1,27 +1,42 @@
 library(tidyverse)
 library(countrycode)
 
-manifesto_raw <- read_csv("manifesto-parties.csv")
-manifesto_share <- read_csv("manifesto-share.csv")
-
-manifesto <- manifesto_raw %>% select(-country) %>% inner_join(manifesto_share)
+manifesto <-
+  read_csv(
+    "source__parties_MPDataset_MPDS2023a.csv",
+    guess_max = 10000,
+    show_col_types = FALSE
+  ) |>
+  mutate(across(c(max_pervote, max_presvote), \(.x) round(.x, 1))) |>
+  select(
+    countryname,
+    party,
+    abbrev,
+    orig_name = name,
+    partyname = name_english,
+    min_year = year_min,
+    max_year = year_max,
+    pervote_max_year = year_max_pervote,
+    pervote_max = max_pervote,
+    presvote_max_year = year_max_presvote,
+    presvote_max = max_presvote,
+    is_alliance
+  )
 
 # add Party Facts country codes
 country_custom <- c(`German Democratic Republic` = "DDR",
                     `Northern Ireland` = "NIR")
 
-manifesto <- manifesto %>%
-  mutate(country = countrycode(
-    countryname, "country.name", "iso3c", custom_match = country_custom
-  ),
-  dummy_nir = if_else(country == "NIR", 1, 0),
-  country = case_when(
-    country == "NIR" ~ "GBR",
-    TRUE ~ country
-  )
+manifesto <-
+  manifesto |>
+  mutate(
+    country = countrycode(countryname, "country.name", "iso3c", custom_match = country_custom),
+    dummy_nir = if_else(country == "NIR", 1, 0),
+    country = case_when(country == "NIR" ~ "GBR",
+                        TRUE ~ country)
   )
 
-if(any(is.na(manifesto$country))) {
+if (any(is.na(manifesto$country))) {
   warning("Country name clean-up needed")
 }
 
