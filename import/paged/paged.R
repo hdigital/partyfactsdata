d01e0d55493f8862cda0fb0bdc0d899cc270073b
pbederke <42916617+pbederke@users.noreply.github.com>
2024-03-04 09:42:06 +0100

    Update paged.R + readme

--- import/paged/paged.R
+++ import/paged/paged.R
@@ -1,44 +1,49 @@
 library(tidyverse)
 library(readxl)
+library(countrycode)
 
-# country codes
-country_code <-
-  tibble(
-    country_id = c(1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,20,21,22,23,24,26,27,28,29,30),
-    countrycode = c("AUT", "BEL", "DNK", "FIN", "FRA", "DEU", "GRC", "ISL", "IRL", "ITA", "NLD", "NOR", "PRT", "ESP", "SWE", "GBR", "BGR", "CZE", "EST", "HUN", "LVA", "LTU", "POL", "ROU", "SVK", "SVN", "HRV")
-  )
+file_in_s <- 
+  read_csv("source__PAGED-basic-party-dataset-Dec-2023.csv", na = "")
 
-# import datasets
-paged_s <- 
-  read_xlsx("source__dataset CEE+Croatia party dataset (string codes).xlsx", na = "") |> 
-  select(party_id_s = party_id)
+file_in_c <-
+  read_xlsx("source__PAGED-basic-party-dataset-Dec-2023.xlsx", na = "") |> 
+  select(party_id)
 
-paged_c <- 
-  read_xlsx("source__dataset WE+CEE+Croatia party dataset (party codes).xlsx", na = "") 
 
-# prepare data
 paged <- 
-  paged_c |> 
-  cbind(paged_s) |> 
-  left_join(country_code, by = "country_id") |> 
+  file_in_s |> 
+  rename(party_name = party_id) |> 
+  bind_cols(file_in_c) |> 
+  select(country_id, party_name, party_id, elecdate, seats_party, seats, partyfacts_id) |> 
   mutate(
-    party_name = if_else(str_length(party_id_s) >= 25, party_id_s, NA_character_),
-    party_id_s = if_else(str_length(party_id_s) >= 25, NA_character_, party_id_s),
-    seat_share = round(party_seats / seats * 100, 1),
-    year = str_extract(elecdate, "[:digit:]{4}"),
-    second_partyfacts_id = if_else(str_detect(partyfacts_id, "\\;"), as.numeric(str_extract(partyfacts_id, "(?<=\\;)[:digit:]*")), NA_real_),
-    partyfacts_id = if_else(str_detect(partyfacts_id, "\\;"), as.numeric(str_extract(partyfacts_id, "[:digit:]*(?=\\;)")), as.numeric(partyfacts_id)),
-  ) |> 
+    countrycode = countrycode(country_id, "country.name", "iso3c"),
+    year = as.numeric(str_extract(elecdate, "[:digit:]{4}")),
+    seat_share = round(seats_party / seats * 100, 1)
+  ) |>
+  select(-country_id, -elecdate, -seats_party, -seats) |> 
   mutate(
-    max_share = max(seat_share, na.rm = F),
     year_first = min(year, na.rm = T),
     year_last = max(year, na.rm = T),
     .by = c(party_id)
   ) |> 
-  filter(seat_share == max_share) |> 
+  arrange(-seat_share) |> 
   slice(1L, .by = c(party_id)) |> 
-  select(countrycode, cab_name, party_id, party_id_s, year_first, year_last, seat_share, party_seats, year, partyfacts_id, second_partyfacts_id) %>% 
-  filter(seat_share >= 1) # high threshold to avoid small parliamentary groups
+  mutate(drop = if_else(str_detect(party_name,"98|99"), 1, 0)) |> 
+  filter(drop == 0) |> 
+  select(-drop) |> 
+  mutate(
+    party_name = case_when(
+      party_id == 30021 ~ NA_character_,
+      party_id == 60011 ~ "DA-FVP",
+      party_id == 70003 ~ "SPAD",
+      party_id == 70005 ~ "OIKEN",
+      T ~ party_name
+    )
+  ) |> 
+  drop_na(party_name) |> 
+  filter(seat_share >= 5) |> 
+  select(countrycode, party_name, party_id, year_first, year_last, seat_share, year, partyfacts_id) |> 
+  distinct()
+
 
 write_csv(paged, "paged.csv", na = "")
-
--- import/paged/readme.md
+++ import/paged/readme.md
@@ -2,13 +2,7 @@
 
 ## Source
 
-Hellström, Johan, Torbjörn Bergman, and Hanna Bäck (2021). Party Government in Europe Database (PAGED). Main sponsor: Riksbankens Jubileumsfond (IN150306:1). Available on https://erdda.org.
-
-Bergman, Torbjörn, Hanna Bäck, and Johan Hellström (eds.). (2021). Coalition Governance in Western Europe. Oxford: Oxford University Press.
-
-Bergman, Torbjörn, Gabriella Ilonszki and Wolfgang C. Müller, eds. (2019). Coalition Governance in Central Eastern Europe. Oxford: Oxford University Press.
-
-https://repdem.org/index.php/party-datasets/
+Hellström, Johan, Torbjörn Bergman, Jonas Lindahl, and Maciej Sychowiec (2023). Party Government in Europe Database (PAGED) – Basic dataset (Party dataset), Version 2023.12. Available on https://repdem.org.
 
 ## Import
 
