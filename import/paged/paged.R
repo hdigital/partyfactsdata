e183cc31ec8e865254cd2a8bf5f6da370ebc3b44
pbederke <42916617+pbederke@users.noreply.github.com>
2024-03-15 10:43:18 +0100

    Revise PAGED

--- import/paged/paged.R
+++ import/paged/paged.R
@@ -1,49 +1,33 @@
 library(tidyverse)
-library(readxl)
+library(readstata13)
 library(countrycode)
 
-file_in_s <- 
+file_in <- 
   read_csv("source__PAGED-basic-party-dataset-Dec-2023.csv", na = "")
-
-file_in_c <-
-  read_xlsx("source__PAGED-basic-party-dataset-Dec-2023.xlsx", na = "") |> 
-  select(party_id)
 
 
 paged <- 
-  file_in_s |> 
-  rename(party_name = party_id) |> 
-  bind_cols(file_in_c) |> 
-  select(country_id, party_name, party_id, elecdate, seats_party, seats, partyfacts_id) |> 
+  file_in |> 
   mutate(
-    countrycode = countrycode(country_id, "country.name", "iso3c"),
+    countrycode = countrycode(country_id_iso, "iso3n", "iso3c"),
     year = as.numeric(str_extract(elecdate, "[:digit:]{4}")),
-    seat_share = round(seats_party / seats * 100, 1)
-  ) |>
-  select(-country_id, -elecdate, -seats_party, -seats) |> 
-  mutate(
+    seat_share = round(seats_party / seats * 100, 1),
     year_first = min(year, na.rm = T),
     year_last = max(year, na.rm = T),
     .by = c(party_id)
   ) |> 
   arrange(-seat_share) |> 
   slice(1L, .by = c(party_id)) |> 
-  mutate(drop = if_else(str_detect(party_name,"98|99"), 1, 0)) |> 
-  filter(drop == 0) |> 
-  select(-drop) |> 
+  filter(seat_share >= 2.5 | !is.na(partyfacts_id)) |> 
+  select(countrycode, party_abbr, party_id, year_first, year_last, seat_share, year, partyfacts_id) |> 
+  distinct() |> 
   mutate(
-    party_name = case_when(
-      party_id == 30021 ~ NA_character_,
-      party_id == 60011 ~ "DA-FVP",
-      party_id == 70003 ~ "SPAD",
-      party_id == 70005 ~ "OIKEN",
-      T ~ party_name
+    countrycode = case_when(
+      countrycode == "THA" ~ "NLD",
+      countrycode == "ARE" ~ "GBR",
+      T ~ countrycode
     )
-  ) |> 
-  drop_na(party_name) |> 
-  filter(seat_share >= 5) |> 
-  select(countrycode, party_name, party_id, year_first, year_last, seat_share, year, partyfacts_id) |> 
-  distinct()
+  )
 
 
 write_csv(paged, "paged.csv", na = "")
