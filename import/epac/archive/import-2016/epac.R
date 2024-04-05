90c2422ab6af9ee0e3f1f71d0bf00b546b3b612c
pbederke <42916617+pbederke@users.noreply.github.com>
2024-04-05 16:40:44 +0200

    Update EPAC
    + use EPAC 2011-17 summary
    + move old files to archive

---
+++ import/epac/archive/import-2016/epac.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(readxl)
+library(countrycode)
+
+epac_raw <- read_excel("epac-parties-2016.xlsx")
+write_csv(epac_raw, "epac-parties-2016.csv", na = "")
+
+# add Party Facts country codes
+epac <- epac_raw %>%
+  mutate(country = countrycode(country_name, "country.name", "iso3c",
+                               custom_match = c(Kosovo="XKX")),
+         seat = round(seat, 1))
+if(any(is.na(epac$country))) {
+  warning("Country name clean-up needed")
+}
+
+epac_2014_raw <- read_csv("import-2014/epac.csv")
+
+epac_2014_add <- epac_2014_raw %>%
+  rename(country = country_name_short, country_name = country, party_id = id,
+         party_accr = accronym, party_name_en = party_name_english) %>%
+  mutate(round = 2011, pec = NA, elecyear = NA) %>%
+  filter( ! party_id %in% epac$party_id)
+
+epac <- epac %>% bind_rows(epac_2014_add) %>% arrange(party_id)
+
+write.csv(epac, "epac.csv", na = "", fileEncoding = "utf-8", row.names = FALSE)
--- import/epac/epac.R
+++ import/epac/epac.R
@@ -1,27 +1,28 @@
 library(tidyverse)
-library(readxl)
 library(countrycode)
 
-epac_raw <- read_excel("epac-parties-2016.xlsx")
-write_csv(epac_raw, "epac-parties-2016.csv", na = "")
+file_in <- read_csv("source__epac 2011-17 summary.csv", na = c("", ".a"))
 
-# add Party Facts country codes
-epac <- epac_raw %>%
-  mutate(country = countrycode(country_name, "country.name", "iso3c",
-                               custom_match = c(Kosovo="XKX")),
-         seat = round(seat, 1))
-if(any(is.na(epac$country))) {
-  warning("Country name clean-up needed")
-}
+epac <- 
+  file_in |> 
+  select(country_name, party_id, party_accr, party_name, party_name_en, elecyear, natstrength, seat, regonly) |> 
+  mutate(
+    countrycode = countrycode(country_name, "country.name", "iso3c", custom_match = c("Kosovo" = "XKX")),
+    share = round(if_else(is.na(natstrength), as.numeric(seat), as.numeric(natstrength)), 2),
+    share_max = max(share, na.rm = T),
+    year_first = min(elecyear, na.rm = T),
+    year_last = max(elecyear, na.rm = T),
+    .by = c("country_name", "party_id")
+  ) |> 
+  filter(share == share_max) |> 
+  mutate(
+    share = if_else(regonly == 1, NA, share),
+    elecyear = if_else(regonly == 1, NA, elecyear)
+  ) |> 
+  slice(1L, .by = party_id) |> 
+  select(countrycode, party_id, name_short = party_accr, name = party_name, name_english = party_name_en, year_first, year_last, share_year = elecyear, share, regonly) |> 
+  arrange(countrycode, party_id)
 
-epac_2014_raw <- read_csv("import-2014/epac.csv")
+epac$party_id |> duplicated() |> any()
 
-epac_2014_add <- epac_2014_raw %>%
-  rename(country = country_name_short, country_name = country, party_id = id,
-         party_accr = accronym, party_name_en = party_name_english) %>%
-  mutate(round = 2011, pec = NA, elecyear = NA) %>%
-  filter( ! party_id %in% epac$party_id)
-
-epac <- epac %>% bind_rows(epac_2014_add) %>% arrange(party_id)
-
-write.csv(epac, "epac.csv", na = "", fileEncoding = "utf-8", row.names = FALSE)
+write_csv(epac, "epac.csv", na = "")
