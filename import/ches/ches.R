4ccf10baf981793ea81295db05d5fb6349a3796b
Paul Bederke <42916617+pbederke@users.noreply.github.com>
2025-03-27 14:17:24 +0100

    Update CHES (#74)
    * Update CHES data
    
    * Update readme.md
    
    * Fix encoding issue
    
    * Fix encoding issue
    
    * Fix encoding (brom issue)
    
    * Exclude codebook information (due to encoding issues)

--- import/ches/ches.R
+++ import/ches/ches.R
@@ -1,30 +1,95 @@
 library(tidyverse)
+library(countrycode)
 
-ches14 <- read_csv("ches-2014/ches.csv")
-ches17 <- read_csv("ches-2017/ches-2017.csv")
-ches19 <- read_csv("ches-2019/ches-2019.csv")
+# Load the data + specify the variables to be used
+ches_countries <- read_csv("ches-party-codebooks.csv", na = "", locale = locale(encoding = "UTF-8")) |> 
+    select(countrycode, countryshort, countryname) |> 
+    distinct()
 
-# combined CHES data
-ches_all <-
-  ches14 %>%
-  select(-cmp_id) %>%
-  bind_rows(ches17) %>%
-  bind_rows(ches19)
+ches_trend_1999_2019 <- read_csv("source__data/1999-2019_CHES_dataset_means(v3).csv", na = "", locale = locale(encoding = "ASCII")) |> 
+    select(countrycode = country, party_id, party, year)
 
-# first and last year
-ches_first_last <-
-  ches_all %>%
-  group_by(party_id) %>%
-  summarise(year_first = min(year_first), year_last = max(year_last))
+ches_candidate_2007 <- read_csv("source__data/2007_CHES-candidates_dataset_means.csv", na = "", locale = locale(encoding = "windows-1252")) |> 
+    select(countrycode = country, party_id, party, year)
 
-# final data for PF import
-ches_out <-
-  ches_all %>%
-  select(-year_first, -year_last) %>%
-  distinct(party_id, .keep_all = TRUE) %>%
-  mutate(electionyear = if_else(is.na(vote), NA_real_, electionyear)) %>%
-  left_join(ches_first_last) %>%
-  select(country_iso3, everything(), -country) %>%
-  arrange(country_iso3, party_name)
+ches_candidate_2014 <- read_csv("source__data/Candidate_Ukraine_2014.csv", na = "", locale = locale(encoding = "ASCII")) |> 
+    select(countrycode = country, party_id, party = party_name) |> 
+    mutate(year = 2014)
 
-write_csv(ches_out, "ches.csv", na = "")
+ches_candidate_2019 <- read_csv("source__data/Mean_Candidate_2019.csv", na = "", locale = locale(encoding = "ASCII")) |> 
+    select(countryname = country, party_id, party) |> 
+    mutate(year = 2019)
+
+ches_flash_2017 <- read_csv("source__data/CHES_means_2017.csv", na = "", locale = locale(encoding = "UTF-8")) |> 
+    select(countryshort = country, party_id, party, year)
+
+ches_covid_2020 <- read_csv("source__data/CHES_COVID.csv", na = "", locale = locale(encoding = "UTF-8")) |> 
+    select(countrycode = country, party_id, party) |> 
+    mutate(year = 2020)
+
+ches_ukraine_2023 <- read_csv("source__data/CHES_Ukraine_March_2024.csv", na = "", locale = locale(encoding = "UTF-8")) |> 
+    select(countryname = country, party_id, party) |> 
+    mutate(year = 2023)
+
+ches_latin_2020 <- read_csv("source__data/ches_la_2020_aggregate_level_v01.csv", na = "", locale = locale(encoding = "windows-1252")) |> 
+    mutate(countryshort = countrycode(country_en, "country.name", "iso3c")) |> 
+    select(countryshort, party_id, party = party_abb) |> 
+    mutate(year = 2020)
+
+ches_israel_2021 <- read_csv("source__data/CHES_ISRAEL_means_2021_2022.csv", na = "", locale = locale(encoding = "ASCII")) |> 
+    mutate(countryshort = "ISR", year = 2021, party = party_name) |> 
+    select(countryshort, year, party_id, party)
+
+# Bind European data
+ches_europe <- 
+    bind_rows(
+        ches_trend_1999_2019 |> left_join(ches_countries, by = c("countrycode")) |> select(countryshort, year, party_id, party),
+        ches_candidate_2007 |> left_join(ches_countries, by = c("countrycode")) |> select(countryshort, year, party_id, party),
+        ches_candidate_2014 |> left_join(ches_countries, by = c("countrycode")) |> select(countryshort, year, party_id, party),
+        ches_candidate_2019 |> mutate(countryshort = countrycode(countryname, "country.name", "iso3c", custom_match = c("Kosovo" = "XKX"))) |> select(countryshort, year, party_id, party),
+        ches_flash_2017 |> mutate(countryshort = countrycode(str_to_upper(countryshort), "iso3c", "iso3c", custom_match = c("CZ" = "CZE", "FR" = "FRA", "GER" = "DEU", "GR" = "GRC", "IT" = "ITA", "NL" = "NLD", "POR" = "PRT", "SLO" = "SVK", "UK" = "GBR"))) |> select(countryshort, year, party_id, party),
+        ches_covid_2020 |> left_join(ches_countries, by = c("countrycode")) |> select(countryshort, year, party_id, party),
+        ches_ukraine_2023 |> mutate(countryshort = countrycode(countryname, "country.name", "iso3c", custom_match = c("Kosovo" = "XKX"))) |> select(countryshort, year, party_id, party),
+    )
+
+# Bind non-European data
+ches_all <- 
+    bind_rows(
+        ches_europe |> mutate(party_id = as.numeric(party_id)),
+        ches_latin_2020 |> mutate(party_id = as.numeric(party_id)),
+        ches_israel_2021 |> mutate(party_id = as.numeric(party_id))
+    )
+
+# Extract party appearances
+ches <- 
+    ches_all |> 
+    mutate(
+        year_first = min(year, na.rm = TRUE),
+        year_last = max(year, na.rm = TRUE),
+        .by = c(party_id)
+    ) |> 
+    select(-year) |> 
+    distinct() |> 
+    slice(1L, .by = c("party_id"))
+
+ches$party_id |> duplicated() |> any()
+
+# Add Party Facts ID (if available)
+partyfacts <- 
+    read_csv("https://partyfacts.herokuapp.com/download/external-parties-csv/", na = "", locale = locale(encoding = "UTF-8")) |> 
+    filter(dataset_key == "ches") |> 
+    select(dataset_party_id, partyfacts_id) |> 
+    mutate(dataset_party_id = as.numeric(dataset_party_id)) |> 
+    distinct()
+
+ches_partyfacts <- 
+    ches |> 
+    left_join(partyfacts, by = c("party_id" = "dataset_party_id"))
+
+encoding_info <- sapply(ches_partyfacts, function(x) {
+    if (is.character(x)) unique(stri_enc_detect(x)[[1]]$Encoding) else NA
+})
+
+print(encoding_info)
+
+write_csv(ches_partyfacts, "ches.csv", na = "")
