1eb4cfb863f7c9b929d8876dd8ee78922552451a
pbederke <42916617+pbederke@users.noreply.github.com>
2025-05-05 10:40:06 +0200

    Update POPPA 2023

--- import/poppa/poppa.R
+++ import/poppa/poppa.R
@@ -1,37 +1,21 @@
 library(tidyverse)
 library(countrycode)
 
-## Create ParlGov-PF-ID link file ----
+raw_poppa <- 
+  read_csv("source__poppa_integrated.csv", na = "NA")
 
-if (FALSE) {
-  # run only manually -- updated PF-IDs not needed after POPPA import into PF
+poppa <- 
+  raw_poppa |> 
+  select(wave, country_short, party_short, party_name_english, party_name_original, poppa_id, partyfacts_id) |> 
+  mutate(
+    year = as.numeric(str_extract(wave, "[:digit:]{4}$")),
+    year_first = min(year, na.rm = T),
+    year_last = max(year, na.rm = T),
+    .by = c("poppa_id")
+  ) |> 
+  select(-wave, -year) |> 
+  distinct()
 
-  url <- "https://partyfacts.herokuapp.com/download/external-parties-csv/"
-
-  pf_external <-
-    read_csv(url, na = "", guess_max = 30000) %>%
-    filter(dataset_key == "parlgov") %>%
-    mutate(parlgov_id = as.integer(dataset_party_id)) %>%
-    select(parlgov_id, partyfacts_id)
-
-  write_csv(pf_external, "partyfacts-external_parlgov.csv")
-}
-
-# Create POPPA PF import ----
-
-raw_poppa_pa <- read_csv("poppa-parties-codebook.csv", na = "")
-raw_poppa_dt <- read_csv("source__party_means.csv", na = "")
-pf_parlgov_link <- read_csv("partyfacts-external_parlgov.csv")
-
-poppa_link <-
-  raw_poppa_dt %>%
-  select(party_id, parlgov_id) %>%
-  left_join(pf_parlgov_link) %>%
-  select(-parlgov_id)
-
-poppa <-
-  raw_poppa_pa %>%
-  mutate(country_short = countrycode(country, "country.name", "iso3c")) %>%
-  left_join(poppa_link)
+poppa |> select(poppa_id) |> duplicated() |> any()
 
 write_csv(poppa, "poppa.csv", na = "")
--- import/poppa/readme.md
+++ import/poppa/readme.md
@@ -1,8 +1,9 @@
-# Populism and Political Parties Expert Survey 2018 (POPPA)
+# Populism and Political Parties Expert Survey (POPPA)
 
 ## Sources
 
-+ Meijers, Maurits; Zaslove, Andrej, 2020, "Populism and Political Parties Expert Survey 2018 (POPPA)", [https://doi.org/10.7910/DVN/8NEL7B](https://doi.org/10.7910/DVN/8NEL7B), Harvard Dataverse, V1
++ Zaslove, Andrej; Meijers, Maurits; Huber, Robert, 2024, "Populism and Political Parties Expert Survey 2023 (POPPA)", [https://doi.org/10.7910/DVN/RMQREQ](https://doi.org/10.7910/DVN/RMQREQ), Harvard Dataverse, V2
++ Zaslove, A., Meijers, M. J., & Huber, R. A. (2024, November 19). The State of Populism: Introducing the 2023 Wave of the Populism and Political Parties Expert Survey. SocArXiv [https://doi.org/10.31235/osf.io/9wmjb](https://doi.org/10.31235/osf.io/9wmjb)
 + [http://poppa-data.eu](http://poppa-data.eu)
 
 ## Import
