0a4aa27e9c833747c9266bfb0b83130efea0adaf
hdigital <3199522+hdigital@users.noreply.github.com>
2025-08-05 09:28:51 +0000

    Reformat R code
    - format with 'styler::style_file()'
    - check code with 'lintr::lint()'

--- import/poppa/poppa.R
+++ import/poppa/poppa.R
@@ -1,21 +1,31 @@
 library(tidyverse)
 library(countrycode)
 
-raw_poppa <- 
+raw_poppa <-
   read_csv("source__poppa_integrated.csv", na = "NA")
 
-poppa <- 
-  raw_poppa |> 
-  select(wave, country_short, party_short, party_name_english, party_name_original, poppa_id) |> 
+poppa <-
+  raw_poppa |>
+  select(
+    wave,
+    country_short,
+    party_short,
+    party_name_english,
+    party_name_original,
+    poppa_id
+  ) |>
   mutate(
     year = as.numeric(str_extract(wave, "[:digit:]{4}$")),
-    year_first = min(year, na.rm = T),
-    year_last = max(year, na.rm = T),
+    year_first = min(year, na.rm = TRUE),
+    year_last = max(year, na.rm = TRUE),
     .by = c("poppa_id")
-  ) |> 
-  select(-wave, -year) |> 
+  ) |>
+  select(-wave, -year) |>
   distinct()
 
-poppa |> select(poppa_id) |> duplicated() |> any()
+poppa |>
+  select(poppa_id) |>
+  duplicated() |>
+  any()
 
 write_csv(poppa, "poppa.csv", na = "")
